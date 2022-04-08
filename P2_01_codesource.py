4c43371c97350d5a7e5165f8450b54db5a99fa28
Stanislas Carlier <stan.carlier@orange.fr>
2022-04-08 20:36:24 +0200

    Resolution message erreur selenium

--- P2_01_codesource.py
+++ P2_01_codesource.py
@@ -1,12 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 
+path = Service('chromedriver_linux64/chromedriver')
+driver = webdriver.Chrome(service=path)
 website = ('https://books.toscrape.com/catalogue/soumission_998/index.html')
-path ='chromedriver_linux64/chromedriver'
-driver = webdriver.Chrome(path)
 driver.get(website)
 
 website_title = driver.find_elements(By.XPATH, '//*[@id="content_inner"]/article/div[1]/div[2]/h1')
+
 titles=[]
 for i in range(len(website_title)):
     title=website_title[i].text
