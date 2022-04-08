4fd9f7bf3398d741ed72cce043835b3690924662
Stanislas Carlier <stan.carlier@orange.fr>
2022-04-08 20:53:58 +0200

    Recuperation Description Livre

--- P2_01_codesource.py
+++ P2_01_codesource.py
@@ -8,11 +8,22 @@ website = ('https://books.toscrape.com/catalogue/soumission_998/index.html')
 driver.get(website)
 
 website_title = driver.find_elements(By.XPATH, '//*[@id="content_inner"]/article/div[1]/div[2]/h1')
+website_desc = driver.find_elements(By.XPATH, '//*[@id="content_inner"]/article/p')
+
+book=[]
+for i in range(len(website_title)):
+    book_info={}
+    book_info['title']=website_title[i].text
+    book_info['description']=website_desc[i].text
+    book.append(book_info)
+print(book)
+
+'''website_title = driver.find_elements(By.XPATH, '//*[@id="content_inner"]/article/div[1]/div[2]/h1')
 
 titles=[]
 for i in range(len(website_title)):
     title=website_title[i].text
     titles.append(title)
-print(titles)
+print(titles)'''
 
 driver.quit()
